30b2153f10db5e9a1c03289fdac1d3831fb58d53
Walter Prince <wprince0288@gmail.com>
2025-08-25 18:09:22 -0500

    landingpage-wip

--- src/components/pages/Landing/index.jsx
+++ src/components/pages/Landing/index.jsx
@@ -1,10 +1,10 @@
-// import pieChart from '../../../assets/pie-chart.png';
-// import lineGraph from '../../../assets/line-graph.png';
-// import barGraph from '../../../assets/bar-graph.png';
-// import paperStack from '../../../assets/paper-stack.jpg';
+import pieChart from '../../../assets/pie-chart.png';
+import lineGraph from '../../../assets/line-graph.png';
+import barGraph from '../../../assets/bar-graph.png';
+import paperStack from '../../../assets/paper-stack.jpg';
 import { useNavigate } from 'react-router-dom';
 import { useDownloadData } from '../../../hooks/useDownloadData.js';
-import {decodeBase64} from '../../../utils/decodeBase64.js';
+import { decodeBase64 } from '../../../utils/decodeBase64.js';
 
 /**
  * TODO: Ticket 1:
@@ -32,8 +32,38 @@ export const LandingPage = () => {
 
   return (
     <div className='flex-c w-[100vw] secondary-c'>
-      Landing Page
-      <div>{'Type this into Canvas: ' + decodeBase64('VGltZTJDb2RlIQ==')}</div>
+      <div>Top</div>
+
+
+      <div className='flex'>
+        <div><img src={paperStack} /></div>
+        <div>Human Rights First has created a search tool to give you a user-friendly way to explore a data set of asylum decisions between FY 2016 and May 2021 by the USCIS Asylum Office, which we received through a Freedom of Information Act request. You can search for information on asylum grant rates by year, nationality, and asylum office, visualize the data with charts and heat maps, and download the data set.</div>
+      </div>
+
+      <div>
+        <div>
+          <h3>
+            Systemic Disparity Insights </h3>
+        </div>
+        <div className='flex'>
+          <div>
+            <h4>36%</h4>
+            <p>By the end of the Trump administration, the average asylum office grant rate had fallen 36% from an average of 44 percent in fiscal year 2016 to 28 percent in fiscal year 20202.</p>
+          </div>
+          <div>
+            <h4>5%</h4>
+            <p>The New York asylum office grant rate dropped to 5 percent in fiscal year 2020.</p>
+          </div>
+          <div>
+            <h4>6x lower</h4>
+            <p>Between fiscal year 2017 and 2020, the New York asylum office's average grant rate was 6 times lower than the San Francisco asylum office.</p>
+          </div>
+        </div>
+
+
+
+      </div>
+
     </div>
   );
 };
